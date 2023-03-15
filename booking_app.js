5ef4326d3ec9616b7f5ad0a48235a31221a7bd7a
darshhbad <darsh.badukle.che17@itbhu.ac.in>
2023-03-15 07:34:45 +0530

    commit 3

--- booking_app.js
+++ booking_app.js
@@ -1,14 +1,26 @@
+// function savetolocal(event){
+//     event.preventDefault();
+//     const name = event.target.name.value;
+//     const date = event.target.date.value;
+//     const phone = event.target.phone.value;
+//     const email = event.target.email.value;
+//     localStorage.setItem('name',name);
+//     localStorage.setItem('phone',phone);
+//     localStorage.setItem('email',email);
+//     localStorage.setItem('date',date);
+// }
 
 function savetolocal(event){
-    event.preventDefault();
-    let obj = {
-        name: event.target.name.value,
-        date: event.target.date.value,
-        phone: event.target.phone.value,
-        email: event.target.email.value
-        }
-        let obj_serial= JSON.stringify(obj);
+     event.preventDefault();
+     let obj = {
+         name: event.target.name.value,
+         date: event.target.date.value,
+         phone: event.target.phone.value,
+         email: event.target.email.value
+         }
+         let obj_serial= JSON.stringify(obj);
 
-        localStorage.setItem("obj",obj_serial);
-
+        localStorage.setItem(obj.email,obj_serial);
 }
+
+
