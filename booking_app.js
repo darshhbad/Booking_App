5b799151ba970226602fd273d01eeac065dabce5
darshhbad <darsh.badukle.che17@itbhu.ac.in>
2023-03-15 15:58:01 +0530

    semi final commit

--- booking_app.js
+++ booking_app.js
@@ -10,17 +10,43 @@
 //     localStorage.setItem('date',date);
 // }
 
-function savetolocal(event){
-     event.preventDefault();
-     let obj = {
-         name: event.target.name.value,
-         date: event.target.date.value,
-         phone: event.target.phone.value,
-         email: event.target.email.value
-         }
-         let obj_serial= JSON.stringify(obj);
+let f1=document.getElementById("name");
+let f2=document.getElementById("phone");
+let f3=document.getElementById("email");
+let uli=document.getElementById("items");
+let forms=document.getElementById("btn")
 
-        localStorage.setItem(obj.email,obj_serial);
+forms.addEventListener('click',local)
+function local(e){
+  e.preventDefault();
+  let myobj={
+    name:f1.value,
+    email:f3.value,
+    phone:f2.value
+  }
+  let parent=document.createElement('li');
+  parent.textContent=myobj.name+" "+myobj.email+" "+myobj.phone;
+  let deletes=document.createElement('button');
+  let text=document.createTextNode("delete");
+  deletes.appendChild(text);
+  parent.appendChild(deletes);
+  uli.appendChild(parent);
+
+  deletes.addEventListener('click',remove)
+  function remove(e){
+    e.preventDefault();
+    localStorage.removeItem(f3.value);
+    uli.removeChild(parent);
+  }
+
+  let myobj_serial=JSON.stringify(myobj);
+  localStorage.setItem(f3.value,myobj_serial);
+
+
+
 }
+
+
+
 
 
